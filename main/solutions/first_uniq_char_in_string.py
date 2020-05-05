b05e187d8787361be4241af0e5f087ee85f3a7cb
techrabbit58 <dzb@pruefbit.de>
2020-05-05 21:16:35 +0200

    first non repeating char performance test

--- main/solutions/first_uniq_char_in_string.py
+++ main/solutions/first_uniq_char_in_string.py
@@ -16,10 +16,12 @@ N o t e
     You may assume the string contain only lowercase letters.
 
 """
+import time
 from collections import Counter
 
 
 def firstUniqChar(s: str) -> int:
+    """Stable runtime with O(n) complexity. Best average and worst case performance."""
     occurrences = Counter(s)
     for k, c in enumerate(s):
         if occurrences[c] == 1:
@@ -27,10 +29,47 @@ def firstUniqChar(s: str) -> int:
     return -1
 
 
+def firstUniqChar_v2(s: str) -> int:
+    """Fast if the first non-repeating char has a low index. Worst case complexity is O(n^2)."""
+    already_seen = set()
+    for k, c in enumerate(s):
+        if c in already_seen:
+            continue
+        if s.count(c) == 1:
+            return k
+    return -1
+
+
+def firstUniqChar_v3(s: str) -> int:
+    """Slowest approach. Worst case complexity is O(n^2). Not much better than v2."""
+    for k, c in enumerate(s):
+        if s.find(c) == s.rfind(c):
+            return k
+    return -1
+
+
 if __name__ == '__main__':
+
     print(firstUniqChar('leetcode'), 0)
     print(firstUniqChar('loveleetcode'), 2)
     print(firstUniqChar('abacabac'), -1)
+
+    start = time.perf_counter_ns()
     print(firstUniqChar('the quick brown fox jumps over the lazy dog'), 4)
+    print(firstUniqChar('bcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba'), 50)
+    print(firstUniqChar('bcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyza'), 50)
+    print('v1', time.perf_counter_ns() - start)
+
+    start = time.perf_counter_ns()
+    print(firstUniqChar_v2('the quick brown fox jumps over the lazy dog'), 4)
+    print(firstUniqChar_v2('bcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyza'), 50)
+    print(firstUniqChar_v2('bcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba'), 50)
+    print('v2', time.perf_counter_ns() - start)
+
+    start = time.perf_counter_ns()
+    print(firstUniqChar_v3('the quick brown fox jumps over the lazy dog'), 4)
+    print(firstUniqChar_v3('bcdefghijklmnopqrstuvwxyzbcdefghijklmnopqrstuvwxyza'), 50)
+    print(firstUniqChar_v3('bcdefghijklmnopqrstuvwxyzzyxwvutsrqponmlkjihgfedcba'), 50)
+    print('v3', time.perf_counter_ns() - start)
 
 # last line of code
