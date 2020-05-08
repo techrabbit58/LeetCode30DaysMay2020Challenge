b2180a446adb8bf3171e80fac87dfc6d4327b7fa
techrabbit58 <dzb@pruefbit.de>
2020-05-08 17:10:56 +0200

    check_if... now with perf counter

--- main/solutions/check_if_it_is_a_straight_line.py
+++ main/solutions/check_if_it_is_a_straight_line.py
@@ -20,6 +20,7 @@ H i n t s
 """
 from typing import List
 from itertools import islice
+from time import perf_counter_ns
 
 
 def checkStraightLine(coordinates: List[List[int]]) -> bool:
@@ -37,10 +38,32 @@ def checkStraightLine(coordinates: List[List[int]]) -> bool:
     return True
 
 
+def checkStraightLine_v2(coordinates: List[List[int]]) -> bool:
+    """This is faster."""
+    if len(coordinates) > 2:
+        (x0, y0), (x1, y1) = coordinates[:2]
+        dx, dy = x0 - x1, y0 - y1
+        return all(dy * (x0 - pn[0]) == dx * (y0 - pn[1]) for pn in islice(coordinates, 2, None))
+    else:
+        return True
+
+
 if __name__ == '__main__':
+
+    start = perf_counter_ns()
     print(checkStraightLine([[-3, 2], [2, 3]]) is True)
     print(checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]) is True)
     print(checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]) is False)
     print(checkStraightLine([[1, 1], [2, 2], [3, 1], [4, 2], [5, 3], [7, 5]]) is False)
+    print(checkStraightLine([[1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]]) is True)
+    print('v1', perf_counter_ns() - start)
+
+    start = perf_counter_ns()
+    print(checkStraightLine_v2([[-3, 2], [2, 3]]) is True)
+    print(checkStraightLine_v2([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]) is True)
+    print(checkStraightLine_v2([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]) is False)
+    print(checkStraightLine_v2([[1, 1], [2, 2], [3, 1], [4, 2], [5, 3], [7, 5]]) is False)
+    print(checkStraightLine_v2([[1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7]]) is True)
+    print('v2', perf_counter_ns() - start)
 
 # last line of code
