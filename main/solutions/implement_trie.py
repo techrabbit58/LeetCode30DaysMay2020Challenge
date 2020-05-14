6bd21decdc0a8b63993ed35818c376f994fcd0bf
techrabbit58 <dzb@pruefbit.de>
2020-05-14 17:55:17 +0200

    implement trie: finished

--- main/solutions/implement_trie.py
+++ main/solutions/implement_trie.py
@@ -33,33 +33,51 @@ A P I   U s a g e
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
+        self.trie = dict()
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
+        """ Inserts a word into the trie. """
+        current = self.trie
+        for ch in word:
+            if ch not in current:
+                current[ch] = dict()
+            current = current[ch]
+        current['*'] = True
+
+    def _search(self, word: str):
+        current = self.trie
+        for ch in word:
+            if ch not in current:
+                return {}
+            current = current[ch]
+        return current
 
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
+        """ Returns if the word is in the trie. """
+        return '*' in self._search(word)
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
+        """ Returns if there is any word in the trie that starts with the given prefix. """
+        return self._search(prefix) != {}
 
 
 if __name__ == '__main__':
     obj = Trie()
     obj.insert('apple')
     print(obj.search('apple') is True)
+    obj.insert('hi')
+    obj.insert('hello')
     print(obj.search('app') is False)
     print(obj.startsWith('app') is True)
     obj.insert('app')
     print(obj.search('app') is True)
+    print(obj.search('hi') is True)
+    print(obj.search('hello') is True)
+    obj.insert('leetcode')
+    print(obj.search('leet') is False)
+    print(obj.search('code') is False)
+    print(obj.search('leetcode') is True)
+    print(obj.startsWith('leet') is True)
+    print(obj.startsWith('h') is True)
 
 # last line of code
